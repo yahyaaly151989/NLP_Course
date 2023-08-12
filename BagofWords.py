d6717d7028c36b39bbc18cdb187a1889118b018c
yahyaaly151989 <122385609+yahyaaly151989@users.noreply.github.com>
2023-08-13 02:40:47 +0800

    Add files via upload

---
+++ BagofWords.py
@@ -0,0 +1,37 @@
+import nltk
+
+paragraph =  """Language is a thing of beauty. But mastering a new language from scratch is quite a daunting prospect.  If you have ever picked up a language that wasnot your mother tongue, 
+you will relate to this! There are so many layers to peel off and syntaxes to consider it is quite a challenge."""
+               
+               
+# Cleaning the texts
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+ps = PorterStemmer()
+wordnet=WordNetLemmatizer()
+sentences = nltk.sent_tokenize(paragraph)
+corpus = []
+for i in range(len(sentences)):
+    review = re.sub('[^a-zA-Z]', ' ', sentences[i])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+print(sentences)
+
+print(X)
+
+
+
+
+
+
